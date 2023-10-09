303a0933e96b173068d4791adf0025c618f2dd15
NguyeLne <94914416+NguyeLne@users.noreply.github.com>
2023-10-09 09:40:38 +0700

    1.0

---
+++ Library/src/main/java/com/ecommerce/library/service/impl/AdminServiceImpl.java
@@ -0,0 +1,36 @@
+package com.ecommerce.library.service.impl;
+
+import com.ecommerce.library.dto.AdminDto;
+import com.ecommerce.library.model.Admin;
+import com.ecommerce.library.repository.AdminRepository;
+import com.ecommerce.library.repository.RoleRepository;
+import com.ecommerce.library.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public Admin findByUserName(String username) {
+        return adminRepository.findByUserName(username);
+    }
+
+    @Override
+    public Admin save(AdminDto adminDto) {
+        Admin admin = new Admin();
+        admin.setFirstName(adminDto.getFirstName());
+        admin.setLastName(adminDto.getLastName());
+        admin.setUserName(adminDto.getUserName());
+        admin.setPassWord(adminDto.getPassWord());
+        admin.setRoles(Arrays.asList(roleRepository.findByName("ADMIN")));
+        return adminRepository.save(admin);
+    }
+}
