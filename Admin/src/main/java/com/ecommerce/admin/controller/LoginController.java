0a81ac3b14b410543ae12bf68270968a54ed8551
Lê Nguyên <nguyenleduc10@gmail.com>
2023-10-12 14:31:46 +0000

    Replace LoginController.java

--- Admin/src/main/java/com/ecommerce/admin/controller/LoginController.java
+++ Admin/src/main/java/com/ecommerce/admin/controller/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
@@ -23,26 +24,36 @@ public class LoginController {
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
     @GetMapping("/login")
-    public String loginForm(){
+    public String loginForm(Model model){
+        model.addAttribute("title","Login Admin");
         return "login";
     }
+
+    @RequestMapping("/index")
+    public String home(Model model){
+        model.addAttribute("title","Home Admin");
+        return "index";
+    }
+
     @GetMapping("/register")
     public String register(Model model){
+        model.addAttribute("title","Dang ky Admin");
         model.addAttribute("adminDto",new AdminDto());
         return "register";
     }
     @GetMapping("/forgot-password")
     public String forgotPassword(Model model){
+        model.addAttribute("title","Quen mat khau");
         return "forgot-password";
     }
 
     @PostMapping("/register-new")
     public String addNewAdmin(@Valid @ModelAttribute("adminDto") AdminDto adminDto,
                               BindingResult result,
-                              Model model,
-                              HttpSession session){
+                              Model model
+                              ){
         try {
-            session.removeAttribute("message");
+//            session.removeAttribute("message");
             if (result.hasErrors()){
                 model.addAttribute("adminDto",adminDto);
                 result.toString();
@@ -52,33 +63,26 @@ public class LoginController {
             Admin admin = adminService.findByUserName(username);
             if (admin != null){
                 model.addAttribute("adminDto",adminDto);
-//                redirectAttributes.addFlashAttribute("message","Email da ton tai");
                 System.out.println("Admin khong rong");
-                session.setAttribute("message","Email da duoc dang ky truoc");
+                model.addAttribute("emailError", "Email da duoc dang ky truoc");
                 return "register";
             }
             if (adminDto.getPassWord().equals(adminDto.getRepPassWord())){
                 adminDto.setPassWord(passwordEncoder.encode(adminDto.getPassWord()));
                 adminService.save(adminDto);
                 System.out.println("Thanh cong");
-                session.setAttribute("message", "Dang ky thanh cong");
+                model.addAttribute("success", "Dang ky thanh cong");
                 model.addAttribute("adminDto",adminDto);
-//                redirectAttributes.addFlashAttribute("message","Dang ky thanh cong");
             }else {
                 model.addAttribute("adminDto",adminDto);
-//                redirectAttributes.addFlashAttribute("message","Mat khau khong trung khop");
-                session.setAttribute("message", "Mat khau khong trung khop");
+                model.addAttribute("passWordError","Mat khau khong trung khop");
                 System.out.println("Mat khau khong trung khop");
                 return "register";
             }
-//            adminService.save(adminDto);
-//            model.addAttribute("adminDto",adminDto);
-//            redirectAttributes.addFlashAttribute("message","Dang ky thanh cong");
-//            return "register";
+
         }catch (Exception e){
-//            redirectAttributes.addFlashAttribute("message","Error Server");
             e.printStackTrace();
-            session.setAttribute("message","Error Server");
+            model.addAttribute("errors","Error Server");
         }
         return "register";
     }
